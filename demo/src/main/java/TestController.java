8a3c32cad8205907404759dbb2bcc1904a1f2403
Thang Doan <thangdoan@ntq-solution.com.vn>
2026-01-28 19:51:54 +0900

    change port

--- demo/src/main/java/TestController.java
+++ demo/src/main/java/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("")
+    @GetMapping("test")
     public String getHello() {
         return "Hello World";
     }
