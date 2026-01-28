0d3ea70743c8c909a01fa0a7769574ddefb10d13
Thang Doan <thangdoan@ntq-solution.com.vn>
2026-01-28 19:57:04 +0900

    change port

--- demo/src/main/java/com/example/demoJenkins/DemoApplication.java
+++ demo/src/main/java/com/example/demoJenkins/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello Jenkins CI 🚀");
 		while (true) {
 			Thread.sleep(1000);
