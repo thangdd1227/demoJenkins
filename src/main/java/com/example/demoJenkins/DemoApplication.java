c28afffeb74c26957a7d944b9235c7febd5cde5f
thangdd <thangdd1227@gmail.com>
2026-03-27 01:09:59 +0700

    aaaa

--- src/main/java/com/example/demoJenkins/DemoApplication.java
+++ src/main/java/com/example/demoJenkins/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello Jenkins CI 🚀");
 		System.out.println("Hello Jenkins CI 🚀");
+		System.out.println("Hello Jenkins CI 🚀");
 		while (true) {
 			Thread.sleep(1000);
 			System.out.println("App is running...");
